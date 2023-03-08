5d7605e85b25dfec339deef9923fb5c4156fed8a
Graham Lyons <s00225961@atu.ie>
2023-03-08 12:28:00 +0000

    app working commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import { OmdbApiService } from './services/omdb-api.service';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [OmdbApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
